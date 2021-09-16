ed0777c3f43436f4d97af3d6b368ae179786554c
Cristian pc <cfromero88@gmail.com>
2021-09-16 14:46:49 -0500

    fslfs

---
+++ routes/historialRuta.js
@@ -0,0 +1,28 @@
+import {
+    Router
+} from "express"
+import {
+    historialRutaNuevaRuta,
+    historialRutaBuscarConductor,
+    historialRutaRefrescarConductor,
+
+    historialRutaListadoPasajeros,
+    historialRutaPasajeroFeedback,
+    historialRutaConductorFeedback,
+    aceptarViajeidConductor,
+    historialRutaBuscarPasajero,
+    historialPasajero
+} from "../controllers/historialRuta.js";
+
+const router = Router();
+router.post('/nuevaruta', historialRutaNuevaRuta);
+router.post('/buscarconductor/:id', historialRutaBuscarConductor);
+router.post('/buscarpasajero', historialRutaListadoPasajeros);
+router.post('/refrescarpasajero', historialRutaBuscarPasajero);
+router.post('/refrescarconductor/:id', historialRutaRefrescarConductor);
+router.post('/conductoraceptaviaje', aceptarViajeidConductor);
+router.post('/pasajero/feedback', historialRutaPasajeroFeedback);
+router.post('/conductor/feedback', historialRutaConductorFeedback);
+router.post('/historialpasajero', historialPasajero)
+
+export default router;
